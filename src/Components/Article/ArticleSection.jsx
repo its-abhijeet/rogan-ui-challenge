8f56f91ea94d03221d850c559196d936c7d50a56
Abhijeet Nigam <basant.abhijeet2003@gmail.com>
2023-05-16 19:20:30 +0530

    Changes for route are done

--- src/Components/Article/ArticleSection.jsx
+++ src/Components/Article/ArticleSection.jsx
@@ -1,10 +1,23 @@
-import React from "react";
-import { data } from "../dummyArticles";
-
+import React, { useEffect, useState } from "react";
+// import { data } from "../dummyArticles";
+import PageLoader from "../common/PageLoader";
 const ArticleSection = () => {
+  const [articlesData, setArticlesData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = () => {
+      setLoading(true);
+      const data = require("../dummyArticles");
+      console.log(data);
+      setArticlesData(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+  if (loading) return <PageLoader />;
   return (
     <div className="mt-5 ">
-      {data.map((article, index) => (
+      {articlesData.data.map((article, index) => (
         <div key={index} className="mb-3">
           <p className="text-xl font-semibold">{article.name}</p>
           <p>{article.description}</p>
--- src/Components/Article/index.jsx
+++ src/Components/Article/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import PageLoader from "../common/PageLoader";
 import SideBar from "./SideBar";
 import { Button } from "antd";
 import { PlusCircleOutlined } from "@ant-design/icons";
 import ArticleSection from "./ArticleSection";
-import NotFound from "../common/NotFound";
 
 const index = () => {
-  return <NotFound />;
+  return (
+    <>
+      <ArticleSection />
+      <SideBar />
+    </>
+  );
 };
 
 export default index;
